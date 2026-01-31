bfa217539b26b38d42cac6df424b14934662c488
iamabdullahalsifat <ads.abdullahalsifat>
2026-01-31 18:21:01 +0600

    update css

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,46 @@
 // ============== Header ==================== //
-let openIcon = document.getElementById('openIcon_');
-let menuIcon = document.querySelector('._navLink');
-let closeIcon = document.querySelector('._closeIcon');
-let closeIconAll = document.querySelectorAll('._navLink li');
+document.addEventListener('DOMContentLoaded', () => {
+  let headerNavContent = document.getElementById('headerNavContent_');
+  let headerNavOpenMenu = document.getElementById('openMenu_');
+  let headerNavCloseMenu = document.getElementById('closeMenu_');
+
+  if(headerNavOpenMenu){
+    headerNavOpenMenu.addEventListener('click', ()=> {
+      headerNavContent.classList.add('show');
+      headerNavOpenMenu.classList.add('active');
+      headerNavCloseMenu.classList.add('active');
+    })
+  }
+
+  if(headerNavCloseMenu){
+    headerNavCloseMenu.addEventListener('click', ()=> {
+      headerNavContent.classList.remove('show');
+      headerNavOpenMenu.classList.remove('active');
+      headerNavCloseMenu.classList.remove('active');
+    })
+  }
+
+})
 
 
-openIcon.addEventListener('click', () =>{
-  menuIcon.classList.add('show')
-})
-closeIcon.addEventListener('click', () => {
-  menuIcon.classList.remove('show')
-});
-closeIconAll.forEach(li => {
-  li.addEventListener('click', () => {
-    menuIcon.classList.remove('show')
-  })
-})
+
+// let openIcon = document.getElementById('openIcon_');
+// let menuIcon = document.querySelector('._navLink');
+// let closeIcon = document.querySelector('._closeIcon');
+// let closeIconAll = document.querySelectorAll('._navLink li');
+
+
+// openIcon.addEventListener('click', () =>{
+//   menuIcon.classList.add('show')
+// })
+// closeIcon.addEventListener('click', () => {
+//   menuIcon.classList.remove('show')
+// });
+// closeIconAll.forEach(li => {
+//   li.addEventListener('click', () => {
+//     menuIcon.classList.remove('show')
+//   })
+// })
 // ============================== //
 
 // ============== FAQ ============== //
